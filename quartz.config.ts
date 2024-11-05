36af223c85f7bca15ec4993c151a9285f83a002c
Jefferson Ferreira-Ferreira <jecogeo@users.noreply.github.com>
2024-11-04 21:19:57 -0300

    pequenas mudanças em título e língua

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,15 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 Jecosfera",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "pt-BR",
+    baseUrl: "jecogeo.eu",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
