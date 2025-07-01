1378c18a3536b91e12039e8bdf4e924a6c00b74e
jecogeo <jecogeo@protonmail.com>
2025-07-01 16:41:34 -0300

    atualizando head.tsx para incluir fathom

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -44,6 +44,26 @@ export default (() => {
         {js
           .filter((resource) => resource.loadTime === "beforeDOMReady")
           .map((res) => JSResourceToScriptElement(res, true))}
+        
+        /* Fathom Lite Analytics */
+	<script
+  dangerouslySetInnerHTML={{
+    __html: `
+      (function(f, a, t, h, o, m){
+        a[h]=a[h]||function(){
+          (a[h].q=a[h].q||[]).push(arguments)
+        };
+        o=f.createElement('script'),
+        m=f.getElementsByTagName('script')[0];
+        o.async=1; o.src='https://fathom.jecogeo.eu/tracker.js'; o.id='fathom-script';
+        m.parentNode.insertBefore(o,m)
+      })(document, window, 'https://fathom.jecogeo.eu/tracker.js', 'fathom');
+      fathom('set', 'siteId', 'QCMTI');
+      fathom('trackPageview');
+    `,
+  }}
+></script>
+
 	</head>
     )
   }
